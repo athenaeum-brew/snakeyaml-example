d143ae6f8a2b360fd11c4c08281bb5288c82171e
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-04-11 17:26:47 +0200

    tweaks

--- src/main/java/com/cthiebaud/SnakeYAMLBeansExample.java
+++ src/main/java/com/cthiebaud/SnakeYAMLBeansExample.java
@@ -2,6 +2,9 @@ package com.cthiebaud;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
+
+import com.cthiebaud.SnakeYAMLBeanExample.NameAndBirthBean;
+
 import org.yaml.snakeyaml.LoaderOptions;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,10 +14,10 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public class SnakeYAMLListExample {
+public class SnakeYAMLBeansExample {
     public static void main(String[] args) throws IOException {
         // Define file path
-        String filePath = "example-list.yaml";
+        String filePath = "example-beans.yaml";
 
         // Create a YAML parser with a custom constructor for NameAndBirthBean class
         Yaml yaml = new Yaml(new Constructor(NameAndBirthBean.class, new LoaderOptions()));
@@ -23,8 +26,9 @@ public class SnakeYAMLListExample {
         Path path = Paths.get(filePath);
         try (InputStream inputStream = Files.newInputStream(path)) {
             // Parse YAML data into a stream of objects representing YAML documents
-            Stream<NameAndBirthBean> stream = StreamSupport.stream(yaml.loadAll(inputStream).spliterator(), false)
-                    .map(NameAndBirthBean.class::cast);
+            Stream<NameAndBirthBean> stream = StreamSupport
+                    .stream(yaml.loadAll(inputStream).spliterator(), false)
+                    .map(SnakeYAMLBeanExample.NameAndBirthBean.class::cast);
 
             // Print loaded entities
             stream.forEach(entity -> {
@@ -35,26 +39,4 @@ public class SnakeYAMLListExample {
         }
     }
 
-    // Java bean class representing Name and Birth attributes
-    public static class NameAndBirthBean {
-        private String name;
-        private String birth;
-
-        // Getters and setters
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getBirth() {
-            return birth;
-        }
-
-        public void setBirth(String birth) {
-            this.birth = birth;
-        }
-    }
 }
--- src/main/java/com/cthiebaud/SnakeYAMLReadExample.java
+++ src/main/java/com/cthiebaud/SnakeYAMLReadExample.java
@@ -1,12 +1,17 @@
 package com.cthiebaud;
 
 import org.yaml.snakeyaml.Yaml;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
+/*
+    mvn exec:java -Dexec.mainClass="com.cthiebaud.SnakeYAMLReadExample"
+*/
 public class SnakeYAMLReadExample {
     public static void main(String[] args) {
         Yaml yaml = new Yaml();
@@ -16,7 +21,7 @@ public class SnakeYAMLReadExample {
 
         // Read data from YAML file
         Path path = Paths.get(filePath);
-        try (var reader = Files.newBufferedReader(path)) {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
             Map<String, Object> data = yaml.load(reader);
             for (Map.Entry<String, Object> entry : data.entrySet()) {
                 System.out.println(entry.getKey() + " = " + entry.getValue());
